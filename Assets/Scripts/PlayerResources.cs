7c08d2fc67acbeb3ca2152b2d433e07d5abe1381
Kyrsulli <kyrsulli@ucsc.edu>
2014-01-15 06:33:12 -0800

    Set up resource variables.
    Set up a script attached to player prefab to contain 3 resources (wood,
    stone, iron) as well as health and fame.
    Created empty method for collecting resources to be filled when resource
    tiles are implemented.
    Note: Unity was rather insistent on automatically deleting Textures.meta
    until I created a folder for textures.

---
+++ Assets/Scripts/PlayerResources.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerResources : MonoBehaviour {
+	public float woodCount = 0;
+	public float stoneCount = 0;
+	public float ironCount = 0;
+	//May end up moving fame and health to their own scripts.
+	public float fameCount = 0;
+	public float healthCount = 0;
+
+	//Once we know how we're doing resource tiles, we can fill out this method.
+	void CollectResources(){
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
