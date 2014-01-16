ded8593c1f12a5a7a591b9be6b01b9012f29ae81
Kyrsulli <kyrsulli@ucsc.edu>
2014-01-16 15:43:18 -0800

    Basic Stat script
    Just committing the basic script that sets up the stats for reference.
    Temp stats are from the prototype.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+	//We might move Health here.
+	//These are just the stat names from the prototype.
+	public float precisionGuns;
+	public float powerGuns;
+	public float sharpWeapon;
+	public float bluntWeapon;
+	public float charm;
+	public float intimidate;
+	public float movement;
+	public float medical;
+	public float crafting;
+	public float repairing;
+	// Use this for initialization
+	void Start () {
+		//Change these once we set up persistant stat levels.
+		precisionGuns = powerGuns = sharpWeapon = bluntWeapon
+			= charm = intimidate = movement = medical = crafting
+				= repairing = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
