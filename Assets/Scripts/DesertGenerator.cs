066a9f1e9cbdf1792228a74a5a72a9c58966fe2e
CodyIsAwesome <cchilder@ucsc.edu>
2014-01-16 23:49:32 -0800

    Simple procedural desert is done!
    Alpha version of the desert is done.  It generates mostly sand with
    little dots of water and stone here and there.  If it gives you an all
    desert landscape, that's not a bug; you just randomly got a shitty
    section of the Perlin Noise plane.  DesertTerrain or whatever I called
    it is sitting in the prefab folder, ready to use.

--- Assets/Scripts/DesertGenerator.cs
+++ Assets/Scripts/DesertGenerator.cs
@@ -23,4 +23,41 @@ public class DesertGenerator : PolygonGenerator {
 	
 	}
 	*/
+
+	/**************************************************************************
+	 * The perlin noise function always returns the same value for same inputs,
+	 * i.e. Mathf.PerlinNoise(5, 10) always has the same return value.
+	 * 
+	 * This function starts by assigning a random value to start at.  THe 
+	 * Perlin Noise plane is infinite, so we will just sample a random portion
+	 * of it.  Loop through and extract the correct value at that point.
+	 * 
+	 * From there, assign ranges.  Low elevations will have water, high will 
+	 * be stone for mountains.  The exact value that constitutes low or high
+	 * will vary based on the apocalypse.  For desert, the threshold for water
+	 * will be super low, but for flood it will be very high
+	 * ***********************************************************************/
+	protected override void GenTerrain(){
+		blocks = new byte[gridWidth, gridHeight];
+		float xStart = Random.Range (-1000, 1000);
+		float yStart = Random.Range (-1000, 1000);
+		for (int px = 0; px < blocks.GetLength (0); px++) {
+			for (int py = 0; py < blocks.GetLength (1); py++) {
+				float xSample = px + xStart / gridWidth;
+				float ySample = py + yStart / gridHeight;
+				//Debug.Log (xSample.ToString () + " " + ySample.ToString ());
+				float elevation = Mathf.PerlinNoise (xSample, ySample); // value 0 - 1
+				//assign values based on this number
+				float waterThreshold = 0.07f;
+				float mountainThreshold = 0.85f;
+				if(elevation < waterThreshold){//low elevation
+					blocks[px, py] = (byte)TileCodes.Water;
+				}else if(elevation >= waterThreshold && elevation < mountainThreshold){//middle elevation
+					blocks[px, py] = (byte)TileCodes.Sand;
+				}else{//high elevation
+					blocks[px, py] = (byte)TileCodes.Stone;
+				}
+			}
+		}
+	}
 }
