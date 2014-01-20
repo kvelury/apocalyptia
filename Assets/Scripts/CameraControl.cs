aa5894dc73852b4f3b67bfd6abc023c069da066e
karatesaul <swiner@ucsc.edu>
2014-01-19 20:22:34 -0800

    Camera Control
    Filled out camera control a bit.  We now have an isometric view where
    the camera follows the player (at an editable distance).  Also messed
    with unity and git a bit more to make it more compatible.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,8 +3,34 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+	/// <summary>
+	/// The velocity of the camera's free movement
+	/// </summary>
+	public Vector3 velocity = new Vector3(10, 10, 0);
+
+	/// <summary>
+	/// The target that the camera follows.
+	/// </summary>
+	public Transform target;
+
+	/// <summary>
+	/// followDistance determines the camera's position relative to the target
+	/// </summary>
+	public Vector3 followDistance = new Vector3 (10, 0, 10);
+
+	/// <summary>
+	/// A boolean to represent the camera's movement mode.
+	/// True represents the camera is attached to the player.
+	/// False represents free-roaming camera.
+	/// </summary>
+	private bool followingTarget = true;
+
 	// Update is called once per frame
 	void Update () {
+		if (followingTarget) {
+			transform.position = target.position - followDistance;
+			transform.LookAt(target);
+		}
 
 	}
 }
