9842b3ff77c9f0b420e5ac548f790a8c6099a5a2
karatesaul <swiner@ucsc.edu>
2014-01-27 22:27:38 -0800

    LookAtScript Polish
    The LookAt script now works properly, pointing everything using it
    towards the camera and with the right face showing.

--- Assets/Scripts/LookAtScript.cs
+++ Assets/Scripts/LookAtScript.cs
@@ -3,18 +3,15 @@ using System.Collections;
 
 public class LookAtScript : MonoBehaviour {
 
-	/// <summary>
-	/// The object the current object should be looking at
-	/// </summary>
-	public Transform target;
+	private Vector3 targetDir;
 
 	// Use this for initialization
 	void Start () {
-	
+		targetDir = GameObject.Find("Main Camera").GetComponent<CameraControl>().followDistance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt (target, new Vector3(0, 0, -1));
+		transform.LookAt (transform.position + targetDir, new Vector3(0, 0, -1));
 	}
 }
