12528485fd740b782882185b07309e86fe8bf747
Kyrsulli <kyrsulli@ucsc.edu>
2014-01-30 20:35:54 -0800

    NPC Dialogue fixed
    NPC dialogue works. Currently has serious issues with "magic numbers"
    (as does my inventory/building menus) that will have to be resolved. All
    HUD elements need to be tied to Screen width/height in the future so
    stuff doesn't look wonky. Currently only displays properly on Maximize
    On Play option. Functions properly. Also, deleted NPC_Dialogue object
    since it was unnecessary and attached NPC_Dialogue script to NPC object.

--- Assets/Scripts/NPC_Dialogue.cs
+++ Assets/Scripts/NPC_Dialogue.cs
@@ -9,6 +9,10 @@ public class NPC_Dialogue : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Questions = new string[3];
+		answerButtons = new string[3];
+		Questions[0] = Questions[1] = Questions[2] = "debug";
+		answerButtons [0] = answerButtons [1] = answerButtons [2] = "debug";
 	
 	}
 	
@@ -18,32 +22,47 @@ public class NPC_Dialogue : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		GUILayout.BeginArea (new Rect (700, 600, 400, 400));
-		if(DisplayDialogue && !ActivateQuest){
-			GUILayout.Label (Questions[0]);
-			GUILayout.Label (Questions[1]);
-			if (GUILayout.Button(answerButtons[0])){
-				ActivateQuest = true;
-				DisplayDialogue = false;
-			}
-			if (GUILayout.Button(answerButtons[1])){
-				DisplayDialogue = false;
-			}
-		}
-		if(DisplayDialogue && ActivateQuest){
-			GUILayout.Label (Questions[2]);
-			if(GUILayout.Button(answerButtons[2])){
-				DisplayDialogue = false;
-			}
-		}
-		GUILayout.EndArea();
+		if (DisplayDialogue) {
+			//We have a serious issue with "Magic Numbers" here. It only displays properly on
+			//"Maximize on Play". We really need to make all the size values dependant on
+			//the screen width.
+						GUI.Box (new Rect (Screen.width /2 - 250, Screen.height - 325, 425, 300), "");//675, 575, 400, 400), "");
+						GUILayout.BeginArea (new Rect (700, 600, 400, 400));
+						if (!ActivateQuest) {
+
+								GUILayout.Label (Questions [0]);
+								GUILayout.Label (Questions [1]);
+								if (GUILayout.Button (answerButtons [0])) {
+										ActivateQuest = true;
+										DisplayDialogue = false;
+								}
+								if (GUILayout.Button (answerButtons [1])) {
+										DisplayDialogue = false;
+								}
+						}
+						if (ActivateQuest) {
+
+								GUILayout.Label (Questions [2]);
+								if (GUILayout.Button (answerButtons [2])) {
+										DisplayDialogue = false;
+								}
+						}
+						GUILayout.EndArea ();
+				}
 	}
 
-	void OnTriggerEnter(){
-		DisplayDialogue = true;
+	void OnTriggerStay2D(Collider2D col){
+		if (col.gameObject.name == "Player")
+						DisplayDialogue = true;
+
 	}
 
-	void OnTriggerExit(){
+	//void OnTriggerEnter(){
+	//	DisplayDialogue = true;
+	//}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.gameObject.name == "Player")
 		DisplayDialogue = false;
 	}
 }
