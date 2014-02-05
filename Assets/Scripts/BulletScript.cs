135db0c03c57a2b1cc134d314f281b4d4860e74b
Kyrsulli <kyrsulli@ucsc.edu>
2014-02-04 16:42:09 -0800

    Attack Tower Functional
    This will be particularly useful if we decide to go the building-focused
    rout. Attack tower now fires a bullet on an interval at the nearest
    enemy. Have not tested it with multiple enemies yet.

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class BulletScript : MonoBehaviour {
+	public Vector3 direction;
+	public float speed;
+	public float damage;
 	// Use this for initialization
 	void Start () {
 		Destroy (gameObject, 1.0F);
+		speed = 10;
+		damage = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		
+	}
+	void FixedUpdate(){
+		rigidbody2D.velocity = (direction * speed);
+	}
+	void OnTriggerStay2D(Collider2D col){
+		if (col.tag == "Enemy") {
+			EnemyStats es = col.gameObject.GetComponent<EnemyStats>();
+			es.HP -= damage;
+			Destroy (gameObject);
+			}
 	}
 }
--- Assets/Scripts/TowerScript.cs
+++ Assets/Scripts/TowerScript.cs
@@ -23,18 +23,28 @@ public class TowerScript : MonoBehaviour {
 
 	}
 
+	void FixedUpdate(){
+		timer++;
+	}
+
 	void OnTriggerStay2D(Collider2D col){
 		if (col.gameObject.tag == "Enemy") {
+			if(timer > 60){
+				timer = 0;
 			//target enemy
 			target = col.transform;
 			//transform.LookAt(target);
 
-			Vector3 direction = new Vector3(target.position.x,target.position.y, 0);
+			Vector3 startPosition = transform.position;//target.position.x,target.position.y, 0);
 			Quaternion rotation = new Quaternion(0,0,0,0);
-			rotation = Quaternion.LookRotation (direction - transform.position);
+			//rotation = Quaternion.LookRotation (direction - transform.position);
+				Vector3 direction = (target.position - transform.position).normalized;
 
-			GameObject newBullet = (GameObject)Instantiate(Bullet, direction, rotation);
-			newBullet.rigidbody2D.AddForce(transform.forward * 5); // shoot in the target direction
+			GameObject newBullet = (GameObject)Instantiate(Bullet, startPosition, rotation);
+				BulletScript bs = newBullet.GetComponent<BulletScript>();
+				bs.direction = direction;
+			//newBullet.rigidbody2D.AddForce(direction * 5); // shoot in the target direction
+			}
 		}
 	}
 
