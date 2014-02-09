911edd4abdeab4d5ee3c8ae2c2a4896049144efd
imacleis <imacleis@ucsc.edu>
2014-02-09 06:58:59 -0800

    Added a solution to to edge of the world
    Just an example of how it can be handled. We can do smoke, fire, magic,
    etc

---
+++ Assets/Scripts/NYI/DeprecatedWASDMovement.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+static var speed : int = 5;
+function Update () {
+       if (Input.GetKey (KeyCode.W)) transform.Translate (Vector3(0,0,1) * Time.deltaTime*speed);
+       if (Input.GetKey (KeyCode.S)) transform.Translate (Vector3(0,0,-1) * Time.deltaTime*speed);
+       if (Input.GetKey (KeyCode.A)) transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
+       if (Input.GetKey (KeyCode.D)) transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
+}
+ 
+ function OnTriggerEnter () 
+{
+	Debug.Log("Thats the end.");
+   	
+}
