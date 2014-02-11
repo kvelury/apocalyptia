c30ef9eca768fcaee924afb9f17c97f5679f0ee2
Kyrsulli <kyrsulli@ucsc.edu>
2014-02-11 09:26:14 -0800

    Changing Weapon Sprite
    Weapon swing sprite is now different depending on whether or not the
    player has found a sword. Will be very easy to switch out for the
    official art when we get it.

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -5,10 +5,18 @@ public class WeaponScript : MonoBehaviour {
 	// Use this for initialization
 
 	private Transform Player;
+	private PlayerStats ps;
+	public Sprite weapon1;
+	public Sprite weapon2;
 
 	void Start () {
 		Destroy (gameObject, .12F);
 		Player = GameObject.Find("Player").transform;
+		ps = GameObject.Find ("Player").GetComponent<PlayerStats> ();
+		if (ps.inv [0] == 0)
+			GetComponent<SpriteRenderer>().sprite = weapon1;
+		else
+			GetComponent<SpriteRenderer>().sprite = weapon2;
 	}
 	
 	// Update is called once per frame
