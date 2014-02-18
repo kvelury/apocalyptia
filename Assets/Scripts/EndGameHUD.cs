5ba7bff15d9dde6976c42cca400b2b24f8b92205
Kyrsulli <kyrsulli@ucsc.edu>
2014-02-18 11:46:56 -0800

    Temporary Endgame Nav
    Since I don't have time to get a full Nav system up in time for the Jim
    meeting, I set up a basic readout of the player's distance to the
    Endgame object, so they can tell if they are getting closer to it. This
    should suffice for now.

--- Assets/Scripts/EndGameHUD.cs
+++ Assets/Scripts/EndGameHUD.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class EndGameHUD : MonoBehaviour {
 	public bool display;
 	public GameObject endgameobj;
+	public GameObject player;
 	// Use this for initialization
 	void Start () {
+		player = GameObject.Find ("Player");
 	
 	}
 	
@@ -16,7 +18,8 @@ public class EndGameHUD : MonoBehaviour {
 
 	void OnGUI(){
 		if (display) {
-			GUI.Box(new Rect(Screen.width/2 - 60, 20, 100, 25), (60 - endgameobj.GetComponent<EndGameScript>().timer).ToString ());
+			GUI.Box(new Rect(Screen.width/2 - 60, 20, 120, 25), (60 - endgameobj.GetComponent<EndGameScript>().timer).ToString ());
+			GUI.Box(new Rect(Screen.width/2 - 120, 65, 240, 25), "Distance to target: " + Mathf.Round(Vector3.Distance (endgameobj.transform.position, player.transform.position)).ToString ());
 		}
 	}
 
