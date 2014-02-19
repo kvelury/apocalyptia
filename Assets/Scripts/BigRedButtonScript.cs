e2cefda5dec32360db9ec29e49a6731d4ef613a4
Kyrsulli <kyrsulli@ucsc.edu>
2014-02-19 06:36:51 -0800

    Big Red Button sound.
    Added sound effects for the Big Red Button; one for flipping open the
    cover, and another for actually clicking the button.

--- Assets/Scripts/BigRedButtonScript.cs
+++ Assets/Scripts/BigRedButtonScript.cs
@@ -10,6 +10,8 @@ public class BigRedButtonScript : MonoBehaviour {
 	public Texture closeLid;
 	public Texture openLid;
 	public Texture currLid;
+	public AudioClip flipOpen;
+	public AudioClip click;
 	// Use this for initialization
 	void Start () {
 		gc = GameObject.Find ("GameController").GetComponent<GameFlowController>();
@@ -24,10 +26,14 @@ public class BigRedButtonScript : MonoBehaviour {
 		if (GUI.Button (new Rect (Screen.width - 220, Screen.height - 220, 200, 200), currLid)) {
 			if(buttonReady == false){
 				buttonReady = true;
+				audio.clip = flipOpen;
+				audio.PlayOneShot (flipOpen);
 				currLid = openLid;
 			}
 			else{
 				Debug.Log ("BRB");
+				audio.clip = click;
+				audio.PlayOneShot(click);
 				gc.NewApocalypse ();
 				buttonReady = false;
 				currLid = closeLid;
