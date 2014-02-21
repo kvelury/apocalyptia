12b1da9277f31d6a5347a987e7e2c2f24bab8c04
capstoneCrusher <mtmcgowa@ucsc.edu>
2014-02-20 23:34:51 -0800

    Lightning Effect
    Isn't visible given current script (and the fact that I removed the
    particle effect from the player because I didn't want to commit the
    scene) But I'll make a second in a second after I sync with the most
    recent build repairing that.

--- Assets/Scripts/HUD/BigRedButtonScript.cs
+++ Assets/Scripts/HUD/BigRedButtonScript.cs
@@ -12,10 +12,18 @@ public class BigRedButtonScript : MonoBehaviour {
 	public Texture currLid;
 	public AudioClip flipOpen;
 	public AudioClip click;
+	public ParticleSystem thunder;
 	// Use this for initialization
 	void Start () {
 		gc = GameObject.Find ("GameController").GetComponent<GameFlowController>();
 		currLid = closeLid;
+		ParticleSystem[] systems = GameObject.Find("Player").GetComponentsInChildren<ParticleSystem>();
+		foreach (ParticleSystem ps in systems){
+			if (ps.tag == "Lightning"){
+				thunder = ps;
+			}
+		}
+		thunder.enableEmission = false;	
 	}
 	
 	// Update is called once per frame
